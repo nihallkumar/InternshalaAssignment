5845dfb1a06a17000b6777dc0b684ef4ebbe2eb1
Nihal Kumar <nihalkumar662001@gmail.com>
2022-12-09 00:42:33 +0530

    update host

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
-        console.log("connected to mongo successfully")
-    });
+    mongoose.connect(
+        mongoURI,
+        (err) => {
+         if(err) console.log(err) 
+         else console.log("mongdb is connected");
+        }
+      );
+    // mongoose.connect(mongoURI, () => {
+    //     console.log("connected to mongo successfully")
+    // });
 }
 
 module.exports = connectToMongo;
